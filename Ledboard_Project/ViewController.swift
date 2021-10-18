72c763d84d4c0c16a1d1a00a28521cdea718c15d
jangsorry <piccolox@naver.com>
2021-10-18 23:48:59 +0900

    Initial Commit

---
+++ Ledboard_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ledboard_Project
+//
+//  Created by 지성 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
